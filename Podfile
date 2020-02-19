441e4a2406eae799b917ceb367b6f27f9a15ab56
Frank Schmitt <frank@apptentive.com>
2020-02-18 16:58:35 -0800

    Initial commit

---
+++ Podfile
@@ -0,0 +1,10 @@
+use_frameworks!
+
+target "Apptentive" do
+  # Normal libraries
+
+  target "ApptentiveTests" do
+    pod 'Quick'
+    pod 'Nimble'
+  end
+end
---
+++ Pods/Target Support Files/Pods-Apptentive-ApptentiveTests/Pods-Apptentive-ApptentiveTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Apptentive_ApptentiveTests : NSObject
+@end
+@implementation PodsDummy_Pods_Apptentive_ApptentiveTests
+@end
