6553ed458914c79007f996f44a57eb331cbfce59
Frank Schmitt <frank@apptentive.com>
2020-02-20 16:42:07 -0800

    Additional comments

--- ApptentiveTests/ApptentiveTests.swift
+++ ApptentiveTests/ApptentiveTests.swift
@@ -10,7 +10,6 @@ import Nimble
 import Quick
 @testable import Apptentive
 
-
 // Test driven approaches:
 // TDD (start with test, implement)
 // BDD (start with a behavior/feature, write test, implement)
@@ -53,6 +52,18 @@ import Quick
 // keys URL request, values URL responses (stateless, readable, maintainable)
 
 // test feature behavior as expected by user
+
+/**
+
+- Dummy objects are passed around but never actually used. Usually they are just used to fill parameter lists.
+- Fake objects actually have working implementations, but usually take some shortcut which makes them not suitable for production (an in memory database is a good example).
+- Stubs provide canned answers to calls made during the test, usually not responding at all to anything outside what's programmed in for the test.
+- Spies are stubs that also record some information based on how they were called. One form of this might be an email service that records how many messages it was sent.
+- Mocks are what we are talking about here: objects pre-programmed with expectations which form a specification of the calls they are expected to receive.
+
+*/
+
+
 class AuthenticationFeatureSpec: QuickSpec {
     
     struct MockAuthenticator: Authenticating {
